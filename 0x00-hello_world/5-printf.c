0579144a517b43aeba189bf386c7efe1e450baf5
7ossam2909 <7ossammostafa.hm@gmail.com>
2022-09-12 14:48:06 -0700

    codding

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programing is like building a multilingual puzzle
-* Return: 0
+ * main- printing line using "printf" command
+ * Description: prints "with proper grammar, but the
+ *              outcome is a piece of art" with a new line at the end
+ * Return: returns 0 if no issue
+ */
+
 int main(void)
+
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+
 }
